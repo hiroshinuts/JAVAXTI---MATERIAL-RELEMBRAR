ef7386cc1f4952633f947b7418372a25cca30a04
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-04-08 21:39:24 -0300

    Math

---
+++ src/nuts/hiro/Math/MathComands.java
@@ -0,0 +1,22 @@
+package nuts.hiro.Math;
+
+public class MathComands {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Math.sin(10)); //seno
+		System.out.println(Math.cos(10)); //cosseno
+		System.out.println(Math.tan(10)); //tangente
+		System.out.println(Math.pow(4, 2)); //potencia
+		System.out.println(Math.sqrt(16)); //raizquadrada
+		System.out.println(Math.cbrt(10)); //raizcubica
+		System.out.println(Math.hypot(6,8)); //hipotenusa
+		
+		
+		//exp - exp(x) = euler elevado a X
+		//ceil - ceil(x) - maior numero inteiro proximo a X
+		//floor - floor(x) - menor numero inteiro proximo a x
+		//round - round(x) - numero arredondado de x
+	}
+
+}
